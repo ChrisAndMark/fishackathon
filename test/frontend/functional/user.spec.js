e19571476f538750d87684003355eac66481c6f7
Mark Niehe <mniehe@gmail.com>
2015-06-26 19:52:58 -0700

    Basic skeleton for testing added

---
+++ test/frontend/functional/user.spec.js
@@ -0,0 +1,8 @@
+var should = require('chai').should();
+	User   = require('../../../frontend/js/app/services/user.service.js');
+	
+	
+describe('User (frontend/js/app/services/user.service.js)', function() {
+	it('does something');
+	
+});
